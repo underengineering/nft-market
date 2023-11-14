e21860ebc8b5c3d64f2a10718d9bbc670f1d5ccb
underengineering <san4a852b@gmail.com>
2023-11-14 17:46:06 +0300

    fix: Use grid in NftOverview

--- src/components/NftOverview.svelte
+++ src/components/NftOverview.svelte
@@ -9,11 +9,18 @@
     $: ownedNfts = nfts.filter((nft) => nft.owner === selectedAccount);
 </script>
 
-<div class="flex flex-col gap-2">
-    <h2 class="text-3xl">Your NFTs</h2>
-    <div class="flex w-full flex-wrap justify-around gap-2">
+<div class="flex w-full flex-col gap-2">
+    <h2 class="text-center text-3xl">Your NFTs</h2>
+    <div id="grid" class="grid w-full justify-evenly">
         {#each ownedNfts as nft}
             <NftCard {nft} />
         {/each}
     </div>
 </div>
+
+<style lang="postcss">
+    #grid {
+        grid-template-columns: repeat(auto-fill, 146px);
+        grid-gap: 1rem;
+    }
+</style>
