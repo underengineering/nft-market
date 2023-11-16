159689ae94bda85ace1a7c405fa28e2cdc552351
underengineering <san4a852b@gmail.com>
2023-11-16 19:30:30 +0300

    feat(CollectionOverview): Add owner check

--- src/components/CollectionOverview.svelte
+++ src/components/CollectionOverview.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { ICollection, INft } from "$lib/contract/icontract";
+    import storage from "$lib/storage";
     import CollectionCard from "./CollectionCard.svelte";
     import EvenGrid from "./EvenGrid.svelte";
 
@@ -9,10 +10,21 @@
     type TCollectionWithNfts = ICollection & { nfts: INft[] };
 
     let ownedCollections: TCollectionWithNfts[];
-    $: ownedCollections = collections.map((collection) => ({
-        ...collection,
-        nfts: nfts.filter((nft) => nft.collectionId === collection.id),
-    }));
+    $: ownedCollections = collections
+        .filter((collection) => {
+            if (collection.isOpen) return false;
+            const firstNft = nfts.find(
+                (nft) => nft.id === collection.nftIds[0]
+            );
+            return (
+                firstNft !== undefined &&
+                firstNft.owner === $storage.selectedAddress
+            );
+        })
+        .map((collection) => ({
+            ...collection,
+            nfts: nfts.filter((nft) => nft.collectionId === collection.id),
+        }));
 </script>
 
 <div class="flex w-full flex-col items-center gap-2">
