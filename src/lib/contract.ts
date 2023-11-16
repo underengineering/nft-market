ed2621624652a1d9c150c78a7255c7a77f07e0c5
underengineering <san4a852b@gmail.com>
2023-11-16 19:48:44 +0300

    feat: Load contract address from .env

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_CONTRACT_ADDRESS } from "$env/static/public";
 import { get } from "svelte/store";
 
 import abi from "./abi";
@@ -5,10 +6,9 @@ import { Contract } from "./contract/contract";
 import storage from "./storage";
 import { web3 } from "./web3";
 
-const CONTRACT_ADDRESS = "0x46718A5600D452dAF65D0d342855b7073DaE50a5";
 export const contract = new Contract(
     get(storage).selectedAddress,
-    new web3.eth.Contract(abi, CONTRACT_ADDRESS)
+    new web3.eth.Contract(abi, PUBLIC_CONTRACT_ADDRESS)
 );
 
 storage.subscribe((value) => (contract.activeAddress = value.selectedAddress));
